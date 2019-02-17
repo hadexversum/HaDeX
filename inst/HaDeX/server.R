8e8c996fe4e76c37734d064815e151c286ff0607
michbur <michalburdukiewicz@gmail.com>
2019-02-17 22:49:19 +0100

    simplified data processing

--- inst/HaDeX/server.R
+++ inst/HaDeX/server.R
@@ -192,7 +192,7 @@ server <- function(input, output, session) {
 
     dat() %>%
       mutate(exp_mass = Center*z - z*proton_mass,
-           Exposure = round(Exposure, 3)) %>%
+             Exposure = round(Exposure, 3)) %>%
       select(-Center, -z, -Protein) %>%
       group_by(Sequence, Start, End, MHP, MaxUptake, State, Exposure, File) %>%
       summarize(avg_exp_mass = weighted.mean(exp_mass, Inten, na.rm = TRUE)) %>%
@@ -202,22 +202,22 @@ server <- function(input, output, session) {
       mutate(theo_in_time_first = (!!sym(chosen_state_first) - MHP)/ (MaxUptake * proton_mass),
              theo_in_time_second = (!!sym(chosen_state_second) - MHP)/(MaxUptake * proton_mass)) %>%
       group_by(Sequence, Start, End) %>%
-      summarize(in_time_mean_first = mean(.[.[["Sequence"]] == Sequence, ][[in_state_first]], na.rm = TRUE),
-                err_in_time_mean_first = coalesce(sd(.[.[["Sequence"]] == Sequence, ][[in_state_first]], na.rm = TRUE), 0),
-                chosen_time_mean_first = mean(.[.[["Sequence"]] == Sequence, ][[chosen_state_first]], na.rm = TRUE),
-                err_chosen_time_mean_first = sd(.[.[["Sequence"]] == Sequence, ][[chosen_state_first]], na.rm = TRUE),
-                out_time_mean_first = mean(.[.[["Sequence"]] == Sequence, ][[out_state_first]], na.rm = TRUE),
-                err_out_time_mean_first = sd(.[.[["Sequence"]] == Sequence, ][[out_state_first]], na.rm = TRUE),
-                in_time_mean_second = mean(.[.[["Sequence"]] == Sequence, ][[in_state_second]], na.rm = TRUE),
-                err_in_time_mean_second = coalesce(sd(.[.[["Sequence"]] == Sequence, ][[in_state_second]], na.rm = TRUE), 0),
-                chosen_time_mean_second = mean(.[.[["Sequence"]] == Sequence, ][[chosen_state_second]], na.rm = TRUE),
-                err_chosen_time_mean_second = sd(.[.[["Sequence"]] == Sequence, ][[chosen_state_second]], na.rm = TRUE),
-                out_time_mean_second = mean(.[.[["Sequence"]] == Sequence, ][[out_state_second]], na.rm = TRUE),
-                err_out_time_mean_second = sd(.[.[["Sequence"]] == Sequence, ][[out_state_second]], na.rm = TRUE),
+      summarize(in_time_mean_first = mean(!!sym(in_state_first), na.rm = TRUE),
+                err_in_time_mean_first = coalesce(sd(!!sym(in_state_first), na.rm = TRUE), 0),
+                chosen_time_mean_first = mean(!!sym(chosen_state_first), na.rm = TRUE),
+                err_chosen_time_mean_first = sd(!!sym(chosen_state_first), na.rm = TRUE),
+                out_time_mean_first = mean(!!sym(out_state_first), na.rm = TRUE),
+                err_out_time_mean_first = sd(!!sym(out_state_first), na.rm = TRUE),
+                in_time_mean_second = mean(!!sym(in_state_second), na.rm = TRUE),
+                err_in_time_mean_second = coalesce(sd(!!sym(in_state_second), na.rm = TRUE), 0),
+                chosen_time_mean_second = mean(!!sym(chosen_state_second), na.rm = TRUE),
+                err_chosen_time_mean_second = sd(!!sym(chosen_state_second), na.rm = TRUE),
+                out_time_mean_second = mean(!!sym(out_state_second), na.rm = TRUE),
+                err_out_time_mean_second = sd(!!sym(out_state_second), na.rm = TRUE),
                 avg_theo_in_time_first = mean(theo_in_time_first, na.rm = TRUE),
                 err_avg_theo_in_time_first = sd(theo_in_time_first, na.rm = TRUE),
                 avg_theo_in_time_second = mean(theo_in_time_second, na.rm = TRUE),
-                err_avg_theo_in_time_second = sd(theo_in_time_second, na.rm = TRUE)) %>%
+                err_avg_theo_in_time_second = sd(theo_in_time_second, na.rm = TRUE))  %>%
       mutate(frac_exch_state_1 = (chosen_time_mean_first - in_time_mean_first)/(out_time_mean_first - in_time_mean_first),
              err_frac_exch_state_1 = sqrt(err_chosen_time_mean_first^2 + 2*err_in_time_mean_first^2 + err_in_time_mean_first^2), 
              frac_exch_state_2 = (chosen_time_mean_second - in_time_mean_second)/(out_time_mean_second - in_time_mean_second),
@@ -227,8 +227,11 @@ server <- function(input, output, session) {
              diff_theo_frac_exch = avg_theo_in_time_first - avg_theo_in_time_second, 
              err_diff_theo_frac_exch = sqrt(err_avg_theo_in_time_first^2 + err_avg_theo_in_time_second^2),
              Med_Sequence = Start + (End - Start)/2) %>%
-      select(Sequence, Start, End, Med_Sequence, frac_exch_state_1, err_frac_exch_state_1, frac_exch_state_2, err_frac_exch_state_2, diff_frac_exch, err_frac_exch, diff_theo_frac_exch, err_diff_theo_frac_exch, avg_theo_in_time_first, avg_theo_in_time_second, err_avg_theo_in_time_first, err_avg_theo_in_time_second) %>%
+      select(Sequence, Start, End, Med_Sequence, frac_exch_state_1, err_frac_exch_state_1, frac_exch_state_2, 
+             err_frac_exch_state_2, diff_frac_exch, err_frac_exch, diff_theo_frac_exch, err_diff_theo_frac_exch, 
+             avg_theo_in_time_first, avg_theo_in_time_second, err_avg_theo_in_time_first, err_avg_theo_in_time_second) %>%
       arrange(Start, End)
+    
     
   })
   
