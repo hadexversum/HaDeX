cd39a083177c0af4989b4d959ee814a3a6cec985
werpuc <puchala.weronika@gmail.com>
2019-02-26 17:03:20 +0100

    changed example file

--- inst/HaDeX/server.R
+++ inst/HaDeX/server.R
@@ -15,7 +15,7 @@ server <- function(input, output, session) {
     inFile <- input[["data_file"]]
     
     if (is.null(inFile)){
-      read.csv('./data/161114_BETA_alpha_gamma.csv')
+      read.csv('./data/KD_180110_CD160_HVEM.csv')
     } else {
       read.csv(inFile[["datapath"]])
     }
