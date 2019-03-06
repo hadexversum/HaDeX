8da38e95bf8e86629b2221f5e739fb24de0cddea
michbur <michalburdukiewicz@gmail.com>
2019-03-06 15:43:33 +0100

    dirty hack to shush RCHECK before the removal of pipes

---
+++ R/HaDeX-package.R
@@ -0,0 +1,11 @@
+if(getRversion() >= "2.15.1")  
+  utils::globalVariables(c('.', 'Center', 'End', 'Exposure', 'File', 'Inten', 'MHP', 'MaxUptake', 
+                           'Med_Sequence', 'Protein', 'Sequence', 'Start', 'State', 'State_Exposure', 
+                           'avg_exp_mass', 'avg_theo_in_time_1', 'avg_theo_in_time_2', 
+                           'chosen_time_mean_1', 'chosen_time_mean_2', 'diff_frac_exch', 'diff_theo_frac_exch', 
+                           'err_avg_theo_in_time_1', 'err_avg_theo_in_time_2', 'err_chosen_time_mean_1', 
+                           'err_chosen_time_mean_2', 'err_diff_theo_frac_exch', 'err_frac_exch', 
+                           'err_frac_exch_state_1', 'err_frac_exch_state_2', 'err_in_time_mean_1', 
+                           'err_in_time_mean_2', 'err_out_time_mean_1', 'err_out_time_mean_2', 
+                           'exp_mass', 'frac_exch_state_1', 'frac_exch_state_2', 
+                           'in_time_mean_1', 'in_time_mean_2', 'out_time_mean_1', 'out_time_mean_2', 'z'))
