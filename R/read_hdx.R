48a96af3b789a28824621a03a410de86b351803c
werpuc <puchala.weronika@gmail.com>
2019-03-15 15:14:26 +0100

    new read_hdx

--- R/read_hdx.R
+++ R/read_hdx.R
@@ -2,8 +2,9 @@
 #' 
 #' Imports data from file and validates its content.
 #' 
+#' @importFrom tools file_ext
 #' @importFrom readxl read_excel
-#' @importFrom readr parse_logical parse_integer parse_double parse_character
+#' @importFrom readr read_csv read_tsv parse_logical parse_integer parse_double parse_character
 #' 
 #' @param filename file supplied by user
 #' 
@@ -16,45 +17,21 @@
 
 read_hdx <- function(filename){
   
-  dat <- if (grepl(".csv", filename)){
-    read.table(filename, sep = ",", header = TRUE, stringsAsFactors = FALSE)
-  } else if (grepl(".tsv", filename)) {
-    read.table(filename, sep = "\t", header = TRUE, stringsAsFactors = FALSE)
-  } else if(grepl(".xls", filename)) {
-    read_excel(filename)
-  }
+  dat <- switch(file_ext(filename),
+                "csv" = read_csv(filename, col_names = TRUE),
+                "tsv" = read_tsv(filename, col_names = TRUE),
+                "xls" = read_excel(filename))
   
-  tryCatch({
+  if (all.equal(colnames(dat), c("Protein", "Start", "End", "Sequence", "Modification", "Fragment", "MaxUptake", "MHP", "State", "Exposure", "File", "z", "RT", "Inten", "Center"))) {
     
-    dat <- mutate(dat,
-                  Protein = parse_character(Protein),
-                  Start = parse_integer(Start),
-                  End = parse_integer(End),
-                  Sequence = parse_character(Sequence),
-                  # Modification = parse_character(Modification),
-                  # Fragment = parse_character(Fragment),
-                  MaxUptake = parse_double(MaxUptake),
-                  MHP = parse_double(MHP),
-                  State = parse_character(State),
-                  Exposure = round(parse_double(Exposure),3),
-                  File = parse_character(File),
-                  z = parse_integer(z),
-                  RT = parse_double(RT),
-                  Inten = parse_double(Inten),
-                  Center = parse_double(Center))
+    dat[["Exposure"]] <- round(dat[["Exposure"]], 3)
     
-  }, warning = function(w){
-
-    print("Check your file")
-
-  }, error = function(e){
-
-    print("Check your file!")
-
-  }, finally = {
-
     dat
-  
-  })
+      
+  } else {
+    
+    stop("Your file doesn\'t meet criteria.")
+    
+  }
   
 }
