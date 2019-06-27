bbd4054f524c641b76a643206cd678f2c19b4435
werpuc <puchala.weronika@gmail.com>
2019-06-27 16:14:18 +0200

    full test coverage

---
+++ tests/testthat/test_plot_position_frequency.R
@@ -0,0 +1,11 @@
+library(testthat)
+library(HaDeX)
+library(vdiffr)
+
+context("plot_position_frequency in test")
+
+dat <- read_hdx(system.file(package = "HaDeX", "HaDeX/data/KD_180110_CD160_HVEM.csv"))
+
+position_frequency_plot_1 <- plot_position_frequency(dat, chosen_state = "CD160_HVEM")
+
+vdiffr::expect_doppelganger("Position Frequency Plot 1", position_frequency_plot_1)
