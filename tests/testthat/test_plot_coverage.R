dd42e2e790044a06cb555f71fa96bc7a64d73736
unknown <ciemnamateria@gmail.com>
2019-08-02 13:23:41 +0200

    unit test for changed plot_coverage fixed

--- tests/testthat/test_plot_coverage.R
+++ tests/testthat/test_plot_coverage.R
@@ -7,7 +7,7 @@ context("plot_coverage in test")
 dat <- read_hdx(system.file(package = "HaDeX", "HaDeX/data/KD_180110_CD160_HVEM.csv"))
 
 test_that("class is right",
-          expect_is(plot_coverage(dat, "CD160_HVEM"),
+          expect_is(plot_coverage(dat, chosen_state = "CD160_HVEM"),
                     "gg"))
 
 # graphic_overlapping_1 <- plot_coverage(dat, "CD160_HVEM")
