92b51a1f4c0c820a88e9113552d395d929ae9507
werpuc <ciemnamateria@gmail.com>
2020-08-31 16:38:37 +0200

    changed variable names in one function + created documentation structure

--- R/calculate_state_deuteration.R
+++ R/calculate_state_deuteration.R
@@ -5,9 +5,9 @@
 #' @param dat data as imported by the \code{\link{read_hdx}} function
 #' @param protein protein included in calculations
 #' @param state state included in calculations
-#' @param time_in experimental `time_in`
-#' @param time_out experimental `time_out`
-#' @param time_chosen chosen time point 
+#' @param time_0 experimental `time_in`
+#' @param time_100 experimental `time_out`
+#' @param time_t chosen time point 
 #' @param deut_part percentage of deuterium the protein was exposed to, value in range [0, 1]
 #' 
 #' @details The function \code{calculate_state_deuteration} calculates deuteration for peptides in given protein in given state based
@@ -34,9 +34,9 @@
 calculate_state_deuteration <- function(dat,
                                         protein, 
                                         state, 
-                                        time_in,
-                                        time_chosen, 
-                                        time_out,
+                                        time_0,
+                                        time_t, 
+                                        time_100,
                                         deut_part = 1
                                         ){
   proton_mass <- 1.00727647
@@ -48,33 +48,38 @@ calculate_state_deuteration <- function(dat,
     group_by(Sequence, Start, End, MHP, MaxUptake, State, Exposure, Protein, File) %>%
     summarize(avg_exp_mass = weighted.mean(exp_mass, Inten, na.rm = TRUE)) %>%
     ungroup(.) %>%
-    mutate(Exposure = case_when(Exposure == time_in ~ "time_in",
-                                Exposure == time_chosen ~ "time_chosen",
-                                Exposure == time_out ~ "time_out")) %>%
+    mutate(Exposure = case_when(Exposure == time_0 ~ "time_in",
+                                Exposure == time_t ~ "time_chosen",
+                                Exposure == time_100 ~ "time_out")) %>%
     spread(key = Exposure, value = avg_exp_mass) %>%
     group_by(Sequence, Start, End, MaxUptake, MHP, Protein, State) %>%
-    summarize(time_in_mean = mean(time_in, na.rm = TRUE),
-              err_time_in_mean = coalesce(sd(time_in, na.rm = TRUE)/sqrt(length(time_in)), 0),
-              time_chosen_mean = mean(time_chosen, na.rm = TRUE),
-              err_time_chosen_mean = coalesce(sd(time_chosen, na.rm = TRUE)/sqrt(length(time_chosen)), 0),
-              time_out_mean = mean(time_out, na.rm = TRUE),
-              err_time_out_mean = coalesce(sd(time_out, na.rm = TRUE)/sqrt(length(time_out)), 0)) %>%
+    summarize(time_0_mean = mean(time_in, na.rm = TRUE),
+              err_time_0_mean = coalesce(sd(time_in, na.rm = TRUE)/sqrt(length(time_in)), 0),
+              time_t_mean = mean(time_chosen, na.rm = TRUE),
+              err_time_t_mean = coalesce(sd(time_chosen, na.rm = TRUE)/sqrt(length(time_chosen)), 0),
+              time_100_mean = mean(time_out, na.rm = TRUE),
+              err_time_100_mean = coalesce(sd(time_out, na.rm = TRUE)/sqrt(length(time_out)), 0)) %>%
     mutate(# experimental calculations below - relative
-      frac_exch_state = 100*(time_chosen_mean - time_in_mean)/(time_out_mean - time_in_mean),
-      err_frac_exch_state = 100*sqrt((err_time_chosen_mean*(1/(time_out_mean - time_in_mean)))^2 + (err_time_in_mean*((time_chosen_mean - time_out_mean )/((time_out_mean - time_in_mean)^2)))^2 + (err_time_out_mean*((time_in_mean - time_chosen_mean)/((time_out_mean - time_in_mean)^2)))^2),
+      frac_deut_uptake = 100*(time_chosen_mean - time_in_mean)/(time_out_mean - time_in_mean),
+      err_frac_deut_uptake = 100*sqrt((err_time_chosen_mean*(1/(time_out_mean - time_in_mean)))^2 + (err_time_in_mean*((time_chosen_mean - time_out_mean )/((time_out_mean - time_in_mean)^2)))^2 + (err_time_out_mean*((time_in_mean - time_chosen_mean)/((time_out_mean - time_in_mean)^2)))^2),
       # experimental calculations below - absolute
-      abs_frac_exch_state = (time_chosen_mean - time_in_mean),
-      err_abs_frac_exch_state = sqrt(err_time_chosen_mean^2 + err_time_in_mean^2),
+      deut_uptake = (time_chosen_mean - time_in_mean),
+      err_deut_uptake = sqrt(err_time_chosen_mean^2 + err_time_in_mean^2),
       # theoretical calculations below - relative
-      avg_theo_in_time = 100*(time_chosen_mean - MHP)/(MaxUptake * proton_mass * deut_part),
-      err_avg_theo_in_time = 100*abs(err_time_chosen_mean)*(1/(MaxUptake * proton_mass * deut_part)),
+      theo_frac_deut_uptake  = 100*(time_chosen_mean - MHP)/(MaxUptake * proton_mass * deut_part),
+      err_theo_frac_deut_uptake  = 100*abs(err_time_chosen_mean)*(1/(MaxUptake * proton_mass * deut_part)),
       # theoeretical calculations below - absolute
-      abs_avg_theo_in_time = (time_chosen_mean - MHP),
-      err_abs_avg_theo_in_time = err_time_chosen_mean,
+      theo_deut_uptake = (time_chosen_mean - MHP),
+      err_theo_deut_uptake = err_time_chosen_mean,
       # helper values
       Med_Sequence = Start + (End - Start)/2) %>%
     ungroup(.) %>%
     arrange(Start, End) %>%
-    select(Protein, Sequence, Start, End, State, frac_exch_state, err_frac_exch_state, abs_frac_exch_state, err_abs_frac_exch_state, avg_theo_in_time, err_avg_theo_in_time,abs_avg_theo_in_time, err_abs_avg_theo_in_time, Med_Sequence)
+    select(Protein, Sequence, Start, End, State, 
+           frac_deut_uptake, err_frac_deut_uptake, 
+           deut_uptake, err_deut_uptake, 
+           theo_frac_deut_uptake, err_theo_frac_deut_uptake,
+           theo_deut_uptake, err_theo_deut_uptake, 
+           Med_Sequence)
 
 }
