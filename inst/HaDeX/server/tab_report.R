6f6b249ab00af097e65a5b53ff93c3fab1a1c0e4
werpuc <puchala.weronika@gmail.com>
2021-08-09 13:09:36 +0200

    information on plot content added #56

--- inst/HaDeX/server/tab_report.R
+++ inst/HaDeX/server/tab_report.R
@@ -1,3 +1,39 @@
+output[["report_message_uptake_curve"]] <- renderText({
+  
+  if(nrow(peptide_list()[input[["peptide_list_data_rows_selected"]], ]) == 0){
+    
+    kin_peptides <- "None"
+    
+  } else {
+    
+    kin_peptides <- paste0(unlist(unique(peptide_list()[input[["peptide_list_data_rows_selected"]], ][["Sequence"]])), collapse = ", ")
+    
+  }
+  
+  paste0("Chosen peptides for uptake curve plot: ", kin_peptides, "\n")
+  
+})
+
+##
+
+output[["report_message_replicate"]] <- renderText({
+  
+  if(nrow(rep_peptide_list()[input[["rep_sequence_rows_selected"]], 2]) == 0){
+    
+    rep_peptide <- "None"
+  
+  } else{
+      
+    rep_peptide <- rep_peptide_list()[input[["rep_sequence_rows_selected"]], 2][[1]]
+    
+  }
+  
+  paste0("Chosen peptide for replicate plot: ", rep_peptide, " \n")
+  
+})
+
+##
+
 output[["export_action"]] <- downloadHandler(
   
   filename <- "HaDeX_Report.html",
