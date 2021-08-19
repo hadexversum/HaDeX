ed9532be0a6e1c6f64c67c7d5f8455413bd52a90
KrystynaGrzesiak <krygrz11@gmail.com>
2021-08-19 12:41:53 +0200

    adds imports and suppressWarnings

--- R/HaDeXify.R
+++ R/HaDeXify.R
@@ -1,7 +1,8 @@
 #' HaDeX customized ggplot theme
 #'
 #' @param plt plot
-#'
+#' @importFrom magick image_read
+#' @importFrom cowplot draw_image ggdraw draw_plot
 #' @export
 
 HaDeXify <- function(plt) {
@@ -9,11 +10,13 @@ HaDeXify <- function(plt) {
   bitmap <- readRDS("./inst/HaDeX/www/HaDeX_logo.RDS")
   img <- image_read(bitmap)
 
-  cowplot::ggdraw() +
-    cowplot::draw_plot(
-      plt +
-        theme(text = element_text(size=16, family="Lato"),
-              legend.position="bottom")) +
-    cowplot::draw_image(img, scale = 0.1, x = 0.44, y = -0.46)
+  suppressWarnings({
+    ggdraw() +
+      draw_plot(
+        plt +
+          theme(text = element_text(size=16, family="Lato"),
+                legend.position="bottom")) +
+      draw_image(img, scale = 0.1, x = 0.44, y = -0.46)
+  })
 }
 
