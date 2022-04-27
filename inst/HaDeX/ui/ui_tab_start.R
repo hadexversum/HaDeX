3461f774bb210f673760d1745b1449f477c2bdd7
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-04-27 11:10:38 +0200

    add mobile version and switching to it

--- inst/HaDeX/ui/ui_tab_start.R
+++ inst/HaDeX/ui/ui_tab_start.R
@@ -1,6 +1,11 @@
-tab_start <- function() HaDeX_nonplotTab(
+tab_start <- function(mobile = FALSE) HaDeX_nonplotTab(
   title = "Start",
   h1("Welcome to HaDeX!"),
+  if (!mobile) NULL else p(
+    tags$b(
+      "For better user experience please use device with wider screen (at least 900px)."
+    )
+  ),
   p(
     "Thank you for using our tool.",
     "Questions/feature requests/commercial applications: hadex@ibb.waw.pl"
