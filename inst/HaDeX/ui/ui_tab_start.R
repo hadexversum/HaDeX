7dc85c0d6e900c732ebfc61c358b3b73296de362
werpuc <puchala.weronika@gmail.com>
2022-05-09 15:25:55 +0200

    small changes and the info

--- inst/HaDeX/ui/ui_tab_start.R
+++ inst/HaDeX/ui/ui_tab_start.R
@@ -1,9 +1,20 @@
 tab_start <- function() HaDeX_nonplotTab(
+  
   title = "Start",
+  
   h1("Welcome to HaDeX!"),
-  p(
-    "Thank you for using our tool.",
-    "Questions/feature requests/commercial applications: hadex@ibb.waw.pl"
+  fluidPage(
+    fluidRow(
+      column(
+        width = 5, 
+        h6("Thank you for using our tool."),
+        h6("Questions/feature requests/commercial applications: hadex@ibb.waw.pl")
+      ),
+      column(
+        width = 5,
+        span("This is the development version of the application and may not be working correctly yet. The official version will be available soon. ", style="color:red")
+      )
+    )
   ),
   includeMarkdown("readmes/about.md"),
   img(
