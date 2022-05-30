67e0d3f2d6a45d764931c670639e29d390017fd0
werpuc <puchala.weronika@gmail.com>
2022-05-30 18:35:41 +0200

    first drafts for funding etc #107

---
+++ R/markdown_texts.R
@@ -0,0 +1,17 @@
+markdown_funding <- function(){
+
+  "This work is supported by Foundation of Polish Science (TEAM TECH CORE FACILITY/2016-2/2 *Mass Spectrometry of Biopharmaceuticals - improved methodologies for qualitative, quantitative and structural characterization of drugs, proteinaceous drug targets and diagnostic molecules)*."
+  
+}
+
+markdown_citation_v1 <- function(){
+  
+  "Puchała, W., Burdukiewicz, M., Kistowski, M., Dąbrowska, K.A., Badaczewska-Dawid, A.E., Cysewski, D., and Dadlez, M. (2020). HaDeX: an R package and web-server for analysis of data from hydrogen–deuterium exchange mass spectrometry experiments. Bioinformatics 36, 4516–4518 (10.1093/bioinformatics/btaa587)."
+  
+}
+
+markown_citation_v2 <- function(){
+  
+  ""
+  
+}
