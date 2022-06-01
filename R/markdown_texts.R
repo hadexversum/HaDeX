9f2ecf1786703ff9a7f98f030e5547b7e030eea5
werpuc <puchala.weronika@gmail.com>
2022-06-01 18:00:28 +0200

    prepared functions for #107

--- R/markdown_texts.R
+++ R/markdown_texts.R
@@ -1,6 +1,11 @@
 markdown_funding <- function(){
 
-  "This work is supported by Foundation of Polish Science (TEAM TECH CORE FACILITY/2016-2/2 *Mass Spectrometry of Biopharmaceuticals - improved methodologies for qualitative, quantitative and structural characterization of drugs, proteinaceous drug targets and diagnostic molecules)*."
+  "This work is supported by:
+  * Foundation of Polish Science (TEAM TECH CORE FACILITY/2016-2/2 *Mass Spectrometry of Biopharmaceuticals - improved methodologies for qualitative, quantitative and structural characterization of drugs, proteinaceous drug targets and diagnostic molecules)*
+  * Narodowe Centrum Nauki (Preludium Bis 1 2019/35/O/NZ2/03745 Wysokorozdzielcza analiza danych z eksperymentów wymiany proton-deuter monitorowanych spektrometrią mas)
+  * Narodowa Agencja Wymiany Akademickiej 
+  
+  "
   
 }
 
@@ -12,6 +17,51 @@ markdown_citation_v1 <- function(){
 
 markown_citation_v2 <- function(){
   
-  ""
+  "Puchała W., ..."
   
 }
+
+markdown_description <- function(){
+  
+  "HaDeX is ..."
+  
+}
+
+markdown_links <- function(){
+  
+  "## Important links
+  
+  * documentation
+  * (package repository)[https://github.com/hadexversum/HaDeX]
+  * standalone software
+  
+  
+  "
+  
+}
+
+markdown_acknowdlegments <- function(){
+  
+  "## Acknowledgements
+  
+  We would like to thank following people, whose work inspired us to make HaDeX better:
+  
+  * David Weis, for his work on uncertainty of propagation.
+  * HDeXplosion team, for introducing chiclet plots to us.
+  * Julien Marcoux, for his valuable insight.
+  * Manhattan plot team, for an idea for new visulization type. 
+  
+  "
+}
+
+markdown_contact <- function(){
+  
+  "## Contact
+  
+  Best way to contact as is through the HaDeX repository. Feel free to open an issue or contact the contributors directly.
+  You can also email us on hadex@ibb.waw.pl.
+  
+  "
+  
+}
+
