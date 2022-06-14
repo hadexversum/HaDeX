88d06776137e11f05407eb02007537c5a817161f
KrystynaGrzesiak <krygrz11@gmail.com>
2022-06-14 12:51:12 +0200

    datatabled new coverage

--- R/plot_coverage.R
+++ R/plot_coverage.R
@@ -3,18 +3,19 @@
 #' @description Plots the peptide coverage of the protein sequence.
 #' 
 #' @importFrom ggplot2 ggplot geom_line labs theme element_blank
-#' @importFrom reshape2 melt
-#' @importFrom dplyr filter arrange
+#' @importFrom plyr . 
 #' 
 #' @param dat data as imported by the \code{\link{read_hdx}} function
 #' @param protein protein to be included in plot
 #' @param states sequence states to be included in plot
 #' 
-#' @details The function \code{plot_coverage} plots sequence coverage based on experimental data for chosen protein in chosen state. 
-#' Only non-duplicated peptides are shown on the plot, next to each other. 
+#' @details The function \code{plot_coverage} plots sequence coverage based on 
+#' experimental data for chosen protein in chosen state. Only non-duplicated 
+#' peptides are shown on the plot, next to each other. 
 #' 
-#' The aim of this plot is to see the dependence between positions of the peptides on the protein sequence. Their position in y-axis 
-#' does not contain any information. 
+#' The aim of this plot is to see the dependence between positions of the 
+#' peptides on the protein sequence. Their position in y-axis does not contain 
+#' any information. 
 #' 
 #' @return a \code{\link[ggplot2]{ggplot}} object.
 #' 
@@ -32,28 +33,73 @@
 #' plot_coverage(dat, protein = "db_CD160", states = "CD160_HVEM")
 #' 
 #' @export plot_coverage
- 
+
 plot_coverage <- function(dat, 
                           protein = dat[["Protein"]][1],
-                          states = dat[["State"]][1]){
+                          states = dat[["State"]][1],
+                          show_blanks = TRUE){
   
-  cov_plot <- dat %>%
-    filter(Protein == protein) %>%
-    select(Start, End, State) %>%
-    filter(State %in% states) %>%
-    select(-State) %>%
-    filter(!duplicated(.)) %>%
-    arrange(Start, End) %>%
-    mutate(ID = 1L:nrow(.)) %>%
-    melt(id.vars = "ID") %>%
-    ggplot(aes(x = value, y = ID, group = ID)) +
-    geom_line() +
+  dat <- dat[Protein == protein & State %in% states, .(Start, End)]
+  dat <- dat[!duplicated(dat)]
+  setorderv(dat, cols = c("Start", "End"))
+  
+  
+  levels <- rep(1, (nrow(dat_tmp)))
+  for(i in 1:(nrow(dat_tmp) - 1)) {
+    if(max(dat_tmp[1:i, "End"]) > dat_tmp[i + 1, "Start"]) {
+      levels[i + 1] <- levels[i] + 1
+    }
+  }
+  
+  dat[, ID := levels]
+  
+  if(show_blanks) {
+    aminos <- rbindlist(lapply(1:nrow(dat), function(i) {
+      amino_acids <- seq(pull(dat[i, "Start"]), pull(dat[i, "End"]), by = 1)
+      data.table(amino_acids)
+    }))
+    missing <- data.frame(ids = setdiff(1:max(aminos$amino_acids), 
+                                        unique(aminos$amino_acids)))
+    
+    non_missing <- data.frame(ids = unique(aminos$amino_acids))
+    
+    
+    coverage_plot <- ggplot(data = dat) +
+      geom_rect(missing,
+                mapping = aes(xmin = ids - 0.5, 
+                              xmax = ids + 0.5,
+                              ymin = 0, 
+                              ymax = max(data_plot[["ID"]])),
+                fill = "#EFB0A1",
+                colour = NA, 
+                alpha = 0.6) +
+      geom_rect(non_missing,
+                mapping = aes(xmin = ids - 0.5, 
+                              xmax = ids + 0.5,
+                              ymin = 0, 
+                              ymax = max(data_plot[["ID"]])),
+                fill = "#C6EBBE",
+                colour = NA, 
+                alpha = 0.6)
+  } else {
+    coverage_plot <- ggplot(data = dat)
+  }
+  
+  coverage_plot <- coverage_plot +
+    geom_rect(data_plot, 
+              mapping = aes(xmin = Start, 
+                            xmax = End, 
+                            ymin = ID, 
+                            ymax = ID - 1), 
+              fill = "#5A748C",
+              colour = "black", 
+              alpha = 0.8) +
+    theme(axis.ticks.y = element_blank(),
+          axis.text.y = element_blank()) +
     labs(title = "Peptide coverage",
          x = "Position",
-         y = "") +
-    theme(axis.ticks.y = element_blank(),
-          axis.text.y = element_blank())
+         y = "")
   
-  return(HaDeXify(cov_plot))
+  return(HaDeXify(coverage_plot))
   
 }
