02acef6e36238514eb4b1bc8b993988cd4f1dc28
KrystynaGrzesiak <krygrz11@gmail.com>
2022-06-21 17:41:23 +0200

    datatable in calculate_state_uptake

--- R/calculate_state_uptake.R
+++ R/calculate_state_uptake.R
@@ -3,6 +3,7 @@
 #' @description Calculates deuteration uptake based on supplied parameters.
 #' 
 #' @importFrom dplyr %>% select mutate group_by coalesce
+#' @importFrom data.table fcase fcoalesce
 #' 
 #' @param dat data as imported by the \code{\link{read_hdx}} function
 #' @param protein protein included in calculations
@@ -48,50 +49,48 @@ calculate_state_uptake <- function(dat,
                                    deut_part = 0.9){
   
   proton_mass <- 1.00727647
-  dat <- dat[dat[["Protein"]] == protein & dat[["State"]] == state & dat[["Exposure"]] %in% c(time_0, time_t, time_100), ]
+  dat <- data.table(dat[dat[["Protein"]] == protein & dat[["State"]] == state & dat[["Exposure"]] %in% c(time_0, time_t, time_100), ])
   
-  uptake_dat <- dat %>%
-    mutate(exp_mass = Center*z - z*proton_mass) %>%
-    select(-Center, -z) %>%
-    group_by(Sequence, Start, End, MHP, MaxUptake, State, Exposure, Protein, File, Modification) %>%
-    summarize(avg_exp_mass = weighted.mean(exp_mass, Inten, na.rm = TRUE)) %>%
-    ungroup(.) %>%
-    mutate(Exposure = case_when(Exposure == time_0 ~ "time_0",
-                                Exposure == time_t ~ "time_t",
-                                Exposure == time_100 ~ "time_100")) %>%
-    spread(key = Exposure, value = avg_exp_mass) %>%
-    group_by(Sequence, Start, End, MaxUptake, MHP, Protein, State, Modification) %>%
-    summarize(time_0_mean = mean(time_0, na.rm = TRUE),
-              err_time_0_mean = coalesce(sd(time_0, na.rm = TRUE)/sqrt(sum(!is.na(time_0))), 0),
-              time_t_mean = mean(time_t, na.rm = TRUE),
-              err_time_t_mean = coalesce(sd(time_t, na.rm = TRUE)/sqrt(sum(!is.na(time_t))), 0),
-              time_100_mean = mean(time_100, na.rm = TRUE),
-              err_time_100_mean = coalesce(sd(time_100, na.rm = TRUE)/sqrt(sum(!is.na(time_100))), 0)) %>%
-    ungroup(.) %>%
-    mutate(# experimental calculations below - fractional
-      frac_deut_uptake = 100*(time_t_mean - time_0_mean)/(time_100_mean - time_0_mean),
-      err_frac_deut_uptake = 100*sqrt((err_time_t_mean*(1/(time_100_mean - time_0_mean)))^2 + (err_time_0_mean*((time_t_mean - time_100_mean )/((time_100_mean - time_0_mean)^2)))^2 + (err_time_100_mean*((time_0_mean - time_t_mean)/((time_100_mean - time_0_mean)^2)))^2),
-      # experimental calculations below - absolute
-      deut_uptake = (time_t_mean - time_0_mean),
-      err_deut_uptake = sqrt(err_time_t_mean^2 + err_time_0_mean^2),
-      # theoretical calculations below - fractional
-      theo_frac_deut_uptake  = 100*(time_t_mean - MHP)/(MaxUptake * proton_mass * deut_part),
-      err_theo_frac_deut_uptake  = 100*abs(err_time_t_mean)*(1/(MaxUptake * proton_mass * deut_part)),
-      # theoeretical calculations below - absolute
-      theo_deut_uptake = (time_t_mean - MHP),
-      err_theo_deut_uptake = err_time_t_mean,
-      # helper values
-      Med_Sequence = Start + (End - Start)/2) %>%
-    ungroup(.) %>%
-    arrange(Start, End) %>%
-    mutate(Exposure = time_t) %>%
-    select(Protein, Sequence, Exposure,
-           Start, End, State, Modification,
-           frac_deut_uptake, err_frac_deut_uptake, 
-           deut_uptake, err_deut_uptake, 
-           theo_frac_deut_uptake, err_theo_frac_deut_uptake,
-           theo_deut_uptake, err_theo_deut_uptake, 
-           Med_Sequence)
+  uptake_dat <- dat[,  `:=`(exp_mass = Center * z - z * proton_mass,
+                            Center = NULL,
+                            z = NULL)]
+  uptake_dat <- uptake_dat[ , .(avg_exp_mass = weighted.mean(exp_mass, Inten, na.rm = TRUE)),
+                            by = c("Sequence", "Start", "End", "MHP", "MaxUptake", 
+                                   "State", "Exposure", "Protein", "File", "Modification")]
+  uptake_dat[, Exposure := fcase(Exposure == time_0, "time_0",
+                                 Exposure == time_t, "time_t",
+                                 Exposure == time_100, "time_100")]
+  uptake_dat <- dcast(uptake_dat, Sequence + Start + End + MHP + MaxUptake + State + Protein + File + Modification + avg_exp_mass ~ Exposure,
+                      value.var = "avg_exp_mass")
+  uptake_dat <- uptake_dat[ , .(time_0_mean = mean(time_0, na.rm = TRUE),
+                                err_time_0_mean = fcoalesce(sd(time_0, na.rm = TRUE)/sqrt(sum(!is.na(time_0))), 0),
+                                time_t_mean = mean(time_t, na.rm = TRUE),
+                                err_time_t_mean = fcoalesce(sd(time_t, na.rm = TRUE)/sqrt(sum(!is.na(time_t))), 0),
+                                time_100_mean = mean(time_100, na.rm = TRUE),
+                                err_time_100_mean = fcoalesce(sd(time_100, na.rm = TRUE)/sqrt(sum(!is.na(time_100))), 0)),
+                            by = c("Sequence", "Start", "End", "MaxUptake", "MHP", "Protein", "State", "Modification")]
+  uptake_dat[, `:=`(frac_deut_uptake = 100*(time_t_mean - time_0_mean)/(time_100_mean - time_0_mean),
+                    err_frac_deut_uptake = 100*sqrt((err_time_t_mean*(1/(time_100_mean - time_0_mean)))^2 + (err_time_0_mean*((time_t_mean - time_100_mean )/((time_100_mean - time_0_mean)^2)))^2 + (err_time_100_mean*((time_0_mean - time_t_mean)/((time_100_mean - time_0_mean)^2)))^2),
+                    # experimental calculations below - absolute
+                    deut_uptake = (time_t_mean - time_0_mean),
+                    err_deut_uptake = sqrt(err_time_t_mean^2 + err_time_0_mean^2),
+                    # theoretical calculations below - fractional
+                    theo_frac_deut_uptake  = 100*(time_t_mean - MHP)/(MaxUptake * proton_mass * deut_part),
+                    err_theo_frac_deut_uptake  = 100*abs(err_time_t_mean)*(1/(MaxUptake * proton_mass * deut_part)),
+                    # theoretical calculations below - absolute
+                    theo_deut_uptake = (time_t_mean - MHP),
+                    err_theo_deut_uptake = err_time_t_mean,
+                    # helper values
+                    Med_Sequence = Start + (End - Start)/2)]
+  setorderv(uptake_dat, cols = c("Start", "End"))
+  uptake_dat[, `:=`(ID = 1L:nrow(uptake_dat),
+                    Exposure = time_t)]
+  uptake_dat <- uptake_dat[, .(Protein, Sequence, Exposure, Start, End, State,
+                               Modification, frac_deut_uptake, err_frac_deut_uptake,
+                               deut_uptake, err_deut_uptake,
+                               theo_frac_deut_uptake, err_theo_frac_deut_uptake,
+                               theo_deut_uptake, err_theo_deut_uptake,
+                               Med_Sequence)]
   
   attr(uptake_dat, "protein") <- protein
   attr(uptake_dat, "state") <- state
@@ -102,5 +101,5 @@ calculate_state_uptake <- function(dat,
   attr(uptake_dat, "has_modification") <- attr(dat, "has_modification")
   
   return(uptake_dat)
-
+  
 }
