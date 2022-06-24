90aeb73f45e155730dbd0d7dd2df3c248ea4cabc
KrystynaGrzesiak <krygrz11@gmail.com>
2022-06-24 09:58:00 +0200

    datatable in calculate_p_value

--- R/calculate_p_value.R
+++ R/calculate_p_value.R
@@ -1,5 +1,7 @@
 #' Create p-value dataset
 #' 
+#' @importFrom data.table setcolorder
+#' 
 #' @param dat data imported by the \code{\link{read_hdx}} function.
 #' @param protein chosen protein. 
 #' @param state_1 biological state for chosen protein. From this state values
@@ -42,36 +44,37 @@ calculate_p_value <- function(dat,
   
   proton_mass <- 1.00727647
   
-    tmp_dat <- dat %>%
-      calculate_exp_masses_per_replicate(.) %>%
-      group_by(Sequence, Start, End, State, Exposure, Modification) %>%
-      summarize(avg_mass = mean(avg_exp_mass),
-                err_avg_mass = sd(avg_exp_mass)/sqrt(length(Exposure)),
-                masses = list(avg_exp_mass)) %>%
-      arrange(Start, End) 
-
-  tmp_dat_1 <- tmp_dat %>%
-    filter(State == state_1) %>%
-    rename(avg_mass_1 = avg_mass,
-           err_avg_mass_1 = err_avg_mass, 
-           masses_1 = masses) %>%
-    ungroup(.) %>%
-    select(-State)
   
-  tmp_dat_2 <- tmp_dat %>%
-    filter(State == state_2) %>%
-    rename(avg_mass_2 = avg_mass,
-           err_avg_mass_2 = err_avg_mass,
-           masses_2 = masses) %>%
-    ungroup(.) %>%
-    select(-State)
+  tmp_dat <- calculate_exp_masses_per_replicate(dat)
+  
+  tmp_dat <- tmp_dat[,  .(avg_mass = mean(avg_exp_mass),
+                          err_avg_mass = sd(avg_exp_mass)/sqrt(uniqueN(.SD)),
+                          masses = list(avg_exp_mass)),
+                     by = c("Sequence", "Start", "End", "State", "Exposure", 
+                            "Modification")]
+  setorderv(tmp_dat, cols = c("Start", "End"))
+  
+  
+  tmp_dat_1 <- tmp_dat[State == state_1]
+  tmp_dat_1 <- setnames(tmp_dat_1,
+                        c("avg_mass", "err_avg_mass", "masses"),
+                        c("avg_mass_1", "err_avg_mass_1", "masses_1"))
+  tmp_dat_1[["State"]] <- NULL
+  
+  
+  tmp_dat_2 <- tmp_dat[State == state_2]
+  tmp_dat_2 <- setnames(tmp_dat_2,
+                        c("avg_mass", "err_avg_mass", "masses"),
+                        c("avg_mass_2", "err_avg_mass_2", "masses_2"))
+  tmp_dat_2[["State"]] <- NULL
+  
   
   vol_dat <- merge(tmp_dat_1, tmp_dat_2, by = c("Sequence", "Start", "End", "Exposure", "Modification"))
+  
+  p_dat <- rbindlist(lapply(1:nrow(vol_dat), function(i){
     
-  p_dat <- lapply(1:nrow(vol_dat), function(i){
-    
-    st_1 <- vol_dat[i, "masses_1"][[1]]
-    st_2 <- vol_dat[i, "masses_2"][[1]]
+    st_1 <- vol_dat[i, "masses_1"][[1]][[1]]
+    st_2 <- vol_dat[i, "masses_2"][[1]][[1]]
     
     if(length(st_1) == 1 | all(st_1 == st_2)) {
       p_value <- NA
@@ -80,23 +83,22 @@ calculate_p_value <- function(dat,
     } else {
       p_value <- t.test(x = st_1, y = st_2, paired = FALSE, alternative = "two.sided", conf.level = confidence_level)$p.value
     }
-
-      data.frame(Protein = protein,
-                 Sequence = vol_dat[i, "Sequence"],
-                 Exposure = vol_dat[i, "Exposure"],
-                 Modification = vol_dat[i, "Modification"],
-                 Start = vol_dat[i, "Start"],
-                 End = vol_dat[i, "End"],
-                 P_value = p_value)
-      
     
-  }) %>% bind_rows() 
+    data.table(Protein = protein,
+               Sequence = vol_dat[i, "Sequence"][[1]],
+               Exposure = vol_dat[i, "Exposure"][[1]],
+               Modification = vol_dat[i, "Modification"][[1]],
+               Start = vol_dat[i, "Start"][[1]],
+               End = vol_dat[i, "End"][[1]],
+               P_value = p_value)
+    
+    
+  }))
   
   p_dat[["P_value"]] <- p.adjust(p_dat[["P_value"]], method = p_adjustment_method)
+  p_dat[["log_p_value"]] <- -log(p_dat[["P_value"]])
   
-  p_dat <- p_dat %>% 
-    mutate(log_p_value = -log(P_value)) %>%
-    arrange(Protein, Start, End)
+  setorderv(p_dat, cols = c("Protein", "Start", "End", "Exposure"))
 
   attr(p_dat, "protein") <- protein
   attr(p_dat, "state_1") <- state_1
